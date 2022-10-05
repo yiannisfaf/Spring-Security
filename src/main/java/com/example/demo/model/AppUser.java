c2517b5d656561942cf23e84f2656ffcf1d93477
Zita Fafalios <zitafafalios@Zitas-MacBook-Pro.local>
2022-10-05 17:16:52 +0100

    init

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -69,14 +69,6 @@ public class AppUser implements UserDetails {
         return email;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
